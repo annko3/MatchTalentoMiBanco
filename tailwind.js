74be4bef2b1099420bfc3d823f77852c873cae5f
Karla Minerva Gala Flores <122924114+Karlagaflo@users.noreply.github.com>
2026-04-15 01:04:36 -0500

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'mi-verde': '#008f39',
+                'mi-amarillo': '#ffdf00',
+                'mi-naranja': '#ff8000',
+            }
+        }
+    }
+}
